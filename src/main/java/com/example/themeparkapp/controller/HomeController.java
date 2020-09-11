2337aabd1de73c7f605556a83a2e4de96190f321
Harsha Vardhan M N <36810886+HarshaV23@users.noreply.github.com>
2020-09-11 10:50:50 +0530

    first commiT!

--- src/main/java/com/example/themeparkapp/controller/HomeController.java
+++ src/main/java/com/example/themeparkapp/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.example.themeparkapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class HomeController {
+    @RequestMapping("/")
     public String sayHello()
     {
         return "hello world docker";
